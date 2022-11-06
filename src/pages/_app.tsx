9ee40aedc360796caa7947ce546f834e5703743f
Matheus <Matheuus412@gmail.com>
2022-11-06 01:44:38 -0300

    :rocket: Ajustes para deploy 1.1

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from "next/app"
 import { globalStyles } from "../styles/global"
-import logoImg from '../assets/logo.svg'
+import logoImg from '../assets/Logo.svg'
 import { Container, Header } from "../styles/pages/app"
 import Image from "next/future/image"
 
